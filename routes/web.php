a1efede86f78ee995a7ccf1482b2ec6313f90de5
Shaiful Islam <kuvic16@gmail.com>
2020-06-18 12:11:41 +0600

    initial commit for laravel practise

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return [
+        'Project' => 'Tornado Vision API',
+        'version' => '1.0'
+    ];
+});
+Route::get('/map', function () {
+    return view('map');
+});
+//https://weathervision.app/api/v1/uid=(UID here),(lat),(lon)
+Route::get('/api/v1', 'ApiController@getReports');
+Route::get('/api/test', 'ApiController@test');
