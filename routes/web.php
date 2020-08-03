426dcb3fd5ceebb5724ae411118068b1c9bc428a
Shaiful Islam <kuvic16@gmail.com>
2020-08-03 23:24:15 +0600

    template included

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,17 @@ Route::get('/', function () {
     // ]);
     // $post->tags()->attach(2);
 
-    return view('home');
+    return view('welcome');
 });
 
 Route::get('/contact', function () {
     return view('contact');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+
 
 Route::get('/post/{post}', "PostController@show");
